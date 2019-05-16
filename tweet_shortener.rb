f9284a6761bc6e24819c0e881851af5d2269c60b
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 21:12:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ def bulk_tweet_shortener(array)
 end
 
 def selective_tweet_shortener(tweet)
-  f tweet.length > 140
+  if tweet.length > 140
     puts word_substituter(tweet)
   else
     puts tweet
