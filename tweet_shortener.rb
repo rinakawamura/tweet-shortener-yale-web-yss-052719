1da907ef99dd35f00263acfa5c420afd8a6411d7
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 20:47:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def dictionary
+  {
+  "hello": 'hi'
+  "to": '2'
+  "two": '2'
+  "too": '2'
+  }
+end
+
+def word_substituter(tweet)
+  words = tweet.split (" ")
+  new_words = []
+  words.each do |word|
+    if dictionary.keys.include?(word)
+      new_words << dictionary[word]
+    else
+      new_words << word
+    end
+  end
+  new_words.join(" ")
+
